cb528b179a2e9ea6785c25d29a2207a16b8bf0f3
COMPANYNET\aprabhakar <kgpavinash@gmail.com>
2018-10-01 11:28:18 -0400

    Add project files.

---
+++ QRCode/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QRCode.Controllers
+{
+    [Route("demo")]
+    public class DemoController : Controller
+    {
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("generate")]
+        public IActionResult Generate(string productId)
+        {
+            ViewBag.productId = productId;
+            return View("Index");
+        }
+    }
+}
